b8c65f88c34a9b469087d95ae704c2052e951541
srikar-codehub <a.srikar.code@gmail.com>
2023-11-02 09:47:10 +0530

    marker_toggle_on_off

--- src/componenets/MapComponent.tsx
+++ src/componenets/MapComponent.tsx
@@ -1,6 +1,8 @@
 import { Feature, Geometry, GeoJsonProperties } from "geojson";
+import { relative } from "path";
 import React, { useState } from "react";
 import Map, { Marker, Source, Layer } from "react-map-gl";
+import { MapMouseEvent } from "react-map-gl/dist/esm/types";
 
 type MarkerType = {
   latitude: number;
@@ -20,52 +22,79 @@ const geojsonData: Feature<Geometry> = {
 
 const MapComponent: React.FC = () => {
   const [viewPort, setViewPort] = useState({
-    longitude: 79.05268345247566,
-    latitude: 21.153949800121893,
+    longitude: 76.47546738176874,
+    latitude: 20.583044348815818,
     zoom: 14,
   });
   const [marker, setMarker] = useState<MarkerType[]>([]);
+  const [isPlacingMarker, setIsPlacingMarker] = useState(false);
+  const handleMarkerClick = (e: any) => {
+    if (!isPlacingMarker) return;
+    setMarker((marker) => [
+      ...marker,
+      {
+        latitude: e.lngLat.lat,
+        longitude: e.lngLat.lng,
+      },
+    ]);
+  };
   return (
-    <Map
-      {...viewPort}
-      style={{ width: "100vw", height: "100vh" }}
-      mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-      mapStyle="mapbox://styles/mapbox/streets-v12"
-      onMove={(e) => setViewPort(e.viewState)}
-      onClick={(e) => {
-        console.log(marker);
-        {
-          setMarker((marker) => [
-            ...marker,
-            {
-              latitude: e.lngLat.lat,
-              longitude: e.lngLat.lng,
-            },
-          ]);
-        }
-      }}
-    >
-      {marker[0] &&
-        marker.map((coordinate) => {
-          return (
-            <Marker
-              latitude={coordinate.latitude}
-              longitude={coordinate.longitude}
-              color="red"
-            ></Marker>
-          );
-        })}
-      <Source type="geojson" data={geojsonData}>
-        <Layer
-          id="line-layer"
-          type="line"
-          paint={{
-            "line-color": "blue",
-            "line-width": 2,
-          }}
-        />
-      </Source>
-    </Map>
+    <div style={{ position: "relative" }}>
+      <button
+        style={{
+          position: "absolute",
+          top: "10px",
+          left: "10px",
+          zIndex: 1,
+          fontSize: "18px",
+          padding: "10px 20px",
+          borderRadius: "8px",
+        }}
+        onClick={() => {
+          console.log(isPlacingMarker);
+          setIsPlacingMarker(!isPlacingMarker);
+        }}
+      >
+        Marker {isPlacingMarker ? "on" : "off"}
+      </button>
+      <Map
+        {...viewPort}
+        style={{ width: "100vw", height: "100vh" }}
+        mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+        mapStyle="mapbox://styles/mapbox/streets-v12"
+        onMove={(e) => {
+          setViewPort(e.viewState);
+        }}
+        onClick={handleMarkerClick}
+      >
+        {marker[0] &&
+          marker.map((coordinate, index) => {
+            return (
+              <Marker
+                key={`marker_${index + 1}`}
+                latitude={coordinate.latitude}
+                longitude={coordinate.longitude}
+                color="red"
+              ></Marker>
+            );
+          })}
+        <Source type="geojson" data={geojsonData}>
+          <Layer
+            id="line-layer"
+            type="line"
+            layout={{
+              "line-cap": "round",
+              "line-join": "round",
+            }}
+            paint={{
+              "line-color": "#ff0000",
+              "line-width": 4,
+              "line-dasharray": [1, 2],
+            }}
+          />
+        </Source>
+      </Map>
+    </div>
   );
 };
 
